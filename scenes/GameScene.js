70a4ef4995d7d62421019d0ca3bb38f826838419
elfivoid <elisa@aulweb.de>
2026-05-08 18:48:09 +0200

    added preload json

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -9,8 +9,12 @@ class GameScene extends Phaser.Scene {
         this.scoreSatisfaction = 50;
     }
      preload ()
-    {
+    {   //preload images and assets
         this.load.image('zones', 'assets/images/zones.png');
+        //preload ticket data
+        this.load.json('ticketData', 'data/tickets.json'); 
+        this.load.json('employees', 'data/employees.json');
+        this.load.json('levels', 'levels.json');
     }
 
      create ()
@@ -23,7 +27,7 @@ class GameScene extends Phaser.Scene {
 //Placeholders
         
         // office desk
-        this.add.rectangle(width / 2, height, width, height * 0.4, 0x808080);
+        let officedesk = this.add.rectangle(width / 2, height, width, height * 0.4, 0x808080);
         
         //screen
         this.monitor = this.add.rectangle(width / 2, height / 2 - 50, 800, 500, 0xffffff);
@@ -34,7 +38,7 @@ class GameScene extends Phaser.Scene {
         // ticket shuffle 
    for (let i = 0; i < 4; i++)
         {
-            const ticket = this.add.rectangle(width / 4, height / 4 - 50, 800, 500, 0xfff234ff, frames[i]);
+            const ticket = this.add.rectangle(width / 4, height / 4 - 50, 800, 500, 0xfff234ff);
             ticket.setStrokeStyle(10, 0x333333);
 
             ticket.setInteractive({ draggable: true });
@@ -51,10 +55,21 @@ class GameScene extends Phaser.Scene {
 
             gameObject.x = dragX;
             gameObject.y = dragY;
-
         });
-    
-   
+
+        // creating a hidden zone for placing the tickets for compare
+        this.add.zone(width / 4, height / 4 - 50, 800, 500, 0xfff234ff);
+
+        //load tickets from json
+        this.tickets = this.cache.json.get('ticketData');
+        this.ticketIndex = 0;
+        //surface to click for spawning tickets with json data
+        let ticketSpawner = this.add.rectangle(960, 900, 200, 60,0x4488ff);
+        ticketSpawner.setInteractive();
+        ticketSpawner.on('pointerdown', () =>{
+            this.showTicket(this.ticketIndex);
+            this.ticketIndex++;
+        });
     }
 
      update ()
