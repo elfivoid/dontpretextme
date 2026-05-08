21a1e8615dbea29bd6501722884522407ba7c7fa
elfivoid <elisa@aulweb.de>
2026-05-08 14:37:09 +0200

    set up a ticket shuffle and ticket drag

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -18,19 +18,40 @@ class GameScene extends Phaser.Scene {
         const {width, height} = this.scale;
         this.add.image(width/2, height/2, 'zones').setDisplaySize(width, height);
 //Placeholders
+        
         // office desk
         this.add.rectangle(width / 2, height, width, height * 0.4, 0x808080);
+        
         //screen
         this.monitor = this.add.rectangle(width / 2, height / 2 - 50, 800, 500, 0xffffff);
         this.monitor.setStrokeStyle(10, 0x333333);
-        //ticket
-        this.ticketText = this.add.text(width / 2, height / 2 - 50, '', {
-            fontSize: '24px',
-            fill: '#000',
-            wordWrap: { width: 700 },
-            align: 'center'
-        }).setOrigin(0.5);
 
+        
+        
+        // ticket shuffle 
+   for (let i = 0; i < 4; i++)
+        {
+            const ticket = this.add.rectangle(width / 4, height / 4 - 50, 800, 500, 0xfff234ff, frames[i]);
+            ticket.setStrokeStyle(10, 0x333333);
+
+            ticket.setInteractive({ draggable: true });
+        }
+
+        this.input.on('dragstart', function (pointer, gameObject) {
+
+            //  This will bring the selected gameObject to the top of the list
+            this.children.bringToTop(gameObject);
+
+        }, this);
+
+        this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+            gameObject.x = dragX;
+            gameObject.y = dragY;
+
+        });
+    
+   
     }
 
      update ()
